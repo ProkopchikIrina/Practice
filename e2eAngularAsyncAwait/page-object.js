650b8b6f6c8f3fb0c0293c56c5c20738f8fe4578
Irina Prokopchik <prokopchik_irina1@mail.ru>
2018-06-13 21:36:05 +0300

    e2e Tests for Angular.io/docs with async/await

---
+++ e2eAngularAsyncAwait/page-object.js
@@ -0,0 +1,50 @@
+class PageObject {
+    get title() {
+        return element(by.css('#what-is-angular'));
+    }
+
+    get search() {
+        return element(by.css('.search-container input'));
+    }
+
+    get leftMenuButton() {
+        return element(by.css('.hamburger.mat-button'));
+    }
+
+    get leftMenu() {
+        return element(by.css('.sidenav.mat-drawer'));
+    }
+
+    get home() {
+        return element(by.css('.nav-link.home'));
+    }
+
+    get leftMenuItemLevel1() {
+        return element(by.css('[title="The fundamentals of Angular"]'));
+    }
+
+    get leftMenuItemLevel2() {
+        return element(by.css('[title="The basic building blocks of Angular applications."]'));
+    }
+
+    get leftMenuGroupOfItemsLevel3() {
+        return element(by.css('.heading-children.level-2'));
+    }
+
+    get mainMenuItem() {
+        return element(by.css('[href="events"]'));
+    }
+
+    get linkChineseLangVersion() {
+        return element(by.css('.link[title="中文版"]'));
+    }
+
+    get mainMenuGroupOfItemsLevel1() {
+        return element(by.css('ul [role="navigation"]'));
+    }
+
+    get linkToHeading() {
+        return element(by.css('.header-link'));
+    }
+}
+module.exports = PageObject;
---
+++ e2eAngularAsyncAwait/spec/spec1.e2e-spec.js
@@ -0,0 +1,110 @@
+var PageObject=require('../page-object.js');
+var pageObject=new PageObject();
+
+describe('search tests', function() {
+    beforeEach(async function() {
+        await  browser.get('http://www.angular.io/docs');
+        await browser.driver.manage().window().maximize();
+    });
+
+    //Проверка вставки в поле поиска
+    it('checks the pasting into search field', async function() {
+        await browser
+            .actions()
+            .mouseMove(pageObject.title,{x:0,y:0})
+            .mouseDown()
+            .mouseMove(pageObject.title,{x:200,y:0})
+            .mouseUp().keyDown(protractor.Key.CONTROL)
+            .sendKeys('c')
+            .perform();
+        await browser.actions().keyDown(protractor.Key.CONTROL).sendKeys('c').perform();
+        await pageObject.search.click();
+        await browser.actions().keyDown(protractor.Key.CONTROL).sendKeys('v').perform();
+        expect(await pageObject.search.getAttribute('value')).toEqual('What is Angular?');
+    });
+
+    //Проверка ввода в поле поиска
+    it('checks the input into search field',async function() {
+        await pageObject.search.click().sendKeys("");
+        await pageObject.search.sendKeys("Search");
+        expect(await pageObject.search.getAttribute('value')).toEqual('Search');
+    });
+
+    //Проверка работы поиска
+    it('checks search',async function() {
+        await pageObject.search.sendKeys("Assumptions");
+        expect(await pageObject.search.getAttribute('.search-results').isDisplayed()).toBeTruthy();
+    });
+
+    //Проверка того, что после клика по другим элементам страницы, поле поиска не очищается
+    it('checks that search field save text after click on anther element of page',async function() {
+        await pageObject.search.sendKeys("Some text");
+        await pageObject.leftMenuItemLevel1.click();
+        expect(await pageObject.search.getAttribute('value')).toEqual('Some text');
+    });
+});
+
+describe('angular.io/docs tests', function() {
+    beforeEach(async function() {
+        await browser.get('http://www.angular.io/docs');
+        await browser.driver.manage().window().maximize();
+    });
+
+    //Проверяет, что при наведении на заголовок, элемент "link to this heading" становится видимым
+    it('checks that after mouse over on title, element "link to this heading" is visible',async function() {
+        await browser.actions().mouseMove(pageObject.title).perform();
+        expect(await pageObject.linkToHeading.isDisplayed()).toBeTruthy();
+    });
+    //Проверка работы кнопки закрывающей боковое меню(закрытие меню) - не проходит
+    it('checks that docs menu button close left menu (closing)',async function() {
+        await pageObject.leftMenuButton.click();
+        expect(await pageObject.leftMenu.isDisplayed()).toBeFalsy();
+    });
+
+    //Проверяет соответсвия текста заголовка ожидаемому
+    it('checks the title',async function() {
+        expect(await pageObject.title.getText()).toEqual('What is Angular?');
+    });
+
+    //Проверка того, что боковое меню не отображается при уменьшении размеров окна браузера
+    it('left menu is not displaying when window size is decreasing',async function() {
+        await browser.driver.manage().window().setSize(1000, 1000);
+        expect(await pageObject.leftMenu.isDisplayed()).toBeFalsy();
+    });
+
+    //Проверка работы кнопки закрывающей боковое меню(открытие меню)
+    it('checks that docs menu button close left menu (opening)',async function() {
+        await pageObject.leftMenuButton.click().click();
+        expect(await pageObject.leftMenu.isDisplayed()).toBeTruthy();
+    });
+
+    //Проверка работы бокового меню
+    it('checks the condition of the left menu by menu items of level-1 and level-2 deployment',async function() {
+        await pageObject.leftMenuItemLevel1.click();
+        await pageObject.leftMenuItemLevel2.click();
+        await pageObject.leftMenuItemLevel1.click().click();
+        expect(await pageObject.leftMenuGroupOfItemsLevel3.isDisplayed()).toBeFalsy();
+    });
+
+    // Проверка работы пункта меню
+    it('checks that menu item works correctly', async function() {
+        await pageObject.mainMenuItem.click();
+        expect(browser.getCurrentUrl()).toEqual('https://angular.io/events');
+    });
+
+    //Проверка работы клика по элементу "link to this heading"
+    it('checks that click on element "link to this heading" works correctly',async function() {
+        expect(await pageObject.linkToHeading.getAttribute('href')).toEqual('https://angular.io/docs#what-is-angular');
+    });
+
+    //Проверка работы элемента 'Home' (переход на главную страницу)
+    it('checks that Home button works correctly',async function () {
+        await pageObject.home.click();
+        expect(await browser.getCurrentUrl()).toEqual('https://angular.io/');
+    });
+
+    //Отображение китайской версии страницы
+    it('Chinese version link', async function() {
+        expect(await pageObject.linkChineseLangVersion.getAttribute('href')).toEqual('https://angular.cn/');
+    });
+});
